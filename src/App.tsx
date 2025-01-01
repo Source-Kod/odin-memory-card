81d49b709298f84fa24c9d4265264530e65e6c61
Source-Kod <Source-Kod@protonmail.com>
2024-12-31 22:36:05 -0600

    add: header & scoreboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   return (
     <>
-      <div className="bg-neutral-500">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <h1>Memory Game</h1>
+        <h3>Click each card, but only once.</h3>
+        <div className="scoreboard flex justify-center gap-4">
+          <p>Current Score: {score} </p>
+          <p>Best Score: {bestScore} </p>
+        </div>
       </div>
-      <h1 className="flex flex-col bg-red-50">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="gameboard"></div>
     </>
   );
 }
